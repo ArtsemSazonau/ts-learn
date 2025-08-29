1424bc061361d06e39ac44c35fb2a3a90aa4bcab
Artsem Sazonau <artsem.sazonau@gmail.com>
2025-08-29 16:52:47 +0300

    add .env; remove example test; update modiles

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('opens homepage and checks title', async ({ page }) => {
-    await page.goto('https://example.com/');
+    await page.goto('/');
     await expect(page).toHaveTitle(/Example Domain/);
 });
 
 test('failed test, check URL', async ({ page }) => {
-    await page.goto('https://example.com/');
+    await page.goto('/');
     await expect(page).toHaveURL('https://playwright.dev/docs/intro');
 });
+
+test('jira credentials check', async () => {
+    console.log('Token: ', process.env.JIRA_API_TOKEN);
+    console.log('Email: ', process.env.JIRA_EMAIL);
+    console.log('URL: ', process.env.JIRA_BASE_URL);
+})
